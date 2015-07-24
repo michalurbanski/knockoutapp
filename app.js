bbb386fa0efc35da3e4be0d51e989af1130af7df
michalurbanski <michal_urbanski@windowslive.com>
2015-07-24 20:40:51 +0200

    Adds simple knockout binding

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 "use strict";
 
 $(document).ready(function(){
-	alert('jquery test');
+	//alert('jquery test');
+	
+	var myModel = {
+		personName: 'Bob', 
+		personAge: 34
+	};
+	
+	ko.applyBindings(myModel);
+	
 });
 
